f81f9a54127533a1898cd677878d5243bade50ed
strubium <113206902+strubium@users.noreply.github.com>
2025-05-26 14:13:07 -0500

    less ass movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,23 +1,20 @@
 extends CharacterBody2D
 
-@export var speed = 12800
-@export var gravity = 550
+@export var speed = 400 
+@export var gravity = 1200 
 @export var terminalVelocity = 1800
-@export var jump_force = 300
+@export var jump_force = 800 
 
 func _physics_process(delta):
-	if !is_on_floor(): ## apply gravity
+	if not is_on_floor():
 		velocity.y += gravity * delta
 		if velocity.y > terminalVelocity:
-				velocity.y = terminalVelocity
-	
-	if Input.is_action_just_pressed("jump"):
-		if is_on_floor():
-			velocity.y = - jump_force
-	
-	## move left and right
+			velocity.y = terminalVelocity
+
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = -jump_force
+
 	var hori_direction = Input.get_axis("move_left", "move_right")
-	velocity.x = speed * hori_direction * delta
+	velocity.x = speed * hori_direction  
 
 	move_and_slide()
-	print(velocity)
