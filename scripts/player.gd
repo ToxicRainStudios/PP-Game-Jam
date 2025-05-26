ae9943bea167bf6c9d84c525d35a664903b6f0b4
strubium <113206902+strubium@users.noreply.github.com>
2025-05-26 13:30:15 -0500

    use delta

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,13 +1,13 @@
 extends CharacterBody2D
 
-@export var speed = 300
-@export var gravity = 30
-@export var terminalVelocity = 600
+@export var speed = 12800
+@export var gravity = 550
+@export var terminalVelocity = 1800
 @export var jump_force = 300
 
 func _physics_process(delta):
 	if !is_on_floor(): ## apply gravity
-		velocity.y += gravity
+		velocity.y += gravity * delta
 		if velocity.y > terminalVelocity:
 				velocity.y = terminalVelocity
 	
@@ -17,7 +17,7 @@ func _physics_process(delta):
 	
 	## move left and right
 	var hori_direction = Input.get_axis("move_left", "move_right")
-	velocity.x = speed * hori_direction
+	velocity.x = speed * hori_direction * delta
 
 	move_and_slide()
 	print(velocity)
