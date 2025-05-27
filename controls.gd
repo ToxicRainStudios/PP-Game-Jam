e599422db0c4d98fa911999648b5f26264046010
strubium <113206902+strubium@users.noreply.github.com>
2025-05-26 19:22:26 -0500

    freaky controls menu

---
+++ controls.gd
@@ -0,0 +1,12 @@
+extends Control
+
+func _ready():
+	var timer = Timer.new()
+	timer.wait_time = 5
+	timer.one_shot = true
+	timer.autostart = true
+	add_child(timer)
+	timer.timeout.connect(self._on_timer_timeout) 
+
+func _on_timer_timeout():
+	get_tree().change_scene_to_file("res://scenes/world.tscn") 
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/world.tscn")  # Updated for Godot 4.x
+	get_tree().change_scene_to_file("res://scenes/controls.tscn") 
 
 func _on_quit_button_pressed():
 	get_tree().quit()
