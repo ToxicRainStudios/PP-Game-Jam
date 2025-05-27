dc4ab2d4551293595678aea385108117fbdf6734
strubium <113206902+strubium@users.noreply.github.com>
2025-05-26 21:40:00 -0500

    ball throwin (ballin)

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,9 @@ extends CharacterBody2D
 @export var gravity = 1200 
 @export var terminalVelocity = 1800
 @export var jump_force = 1000
+@export var ball_scene: PackedScene
+@export var throw_force = Vector2(600, -400)
+
 
 @onready var sprite: Sprite2D = $Sprite2D
 @onready var animator : AnimationPlayer = $Sprite2D/AnimationPlayer
@@ -32,6 +35,7 @@ func _physics_process(delta):
 		print("hi")
 	if Input.is_action_just_released("possess"):
 		animator.play("release")
+		_throw_ball()
 		
 
 	velocity.x = speed * hori_direction  
@@ -41,3 +45,13 @@ func _physics_process(delta):
 		sprite.flip_h = hori_direction < 0
 
 	move_and_slide()
+
+func _throw_ball():
+	if ball_scene:
+		var ball = ball_scene.instantiate() as RigidBody2D
+		var direction = 1
+		if sprite.flip_h:
+			direction = -1
+		get_parent().add_child(ball)
+		ball.global_position = global_position + Vector2(20 * direction, -10)
+		ball.apply_impulse(Vector2(throw_force.x * direction, throw_force.y))
