8c142aa42f3d1cdb7380def18a1d4f9749f91683
Gabefry <167709107+Gabefry@users.noreply.github.com>
2025-05-26 20:54:07 -0500

    animations

--- scripts/knight.gd
+++ scripts/knight.gd
@@ -6,16 +6,22 @@ var direct = 1
 
 @onready var ray_cast_left: RayCast2D = $RayCast2D2
 @onready var ray_cast_right: RayCast2D = $RayCast2D
-@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var sprite: Sprite2D = $Sprite2D
 
 
 func _process(delta):
 	if ray_cast_right.is_colliding():
-		direct = -1
-		sprite_2d.flip_h= true
+		if (ray_cast_right.get_collider() == $"../../player"):
+			print("touching player")
+		else:
+			direct = -1
+			sprite.flip_h= true
 	if ray_cast_left.is_colliding():
-		direct= 1
-		sprite_2d.flip_h= false
+		if (ray_cast_left.get_collider() == $"../../player"):
+			print("touching player")
+		else:
+			direct = 1
+			sprite.flip_h= false
 	
 	position.x += direct* SPEED * delta
 	
--- scripts/player.gd
+++ scripts/player.gd
@@ -3,7 +3,11 @@ extends CharacterBody2D
 @export var speed = 400 
 @export var gravity = 1200 
 @export var terminalVelocity = 1800
-@export var jump_force = 16000
+@export var jump_force = 1000
+
+@onready var sprite: Sprite2D = $Sprite2D
+@onready var animator : AnimationPlayer = $Sprite2D/AnimationPlayer
+
 
 @onready var sprite = $Sprite2D
 
@@ -14,9 +18,22 @@ func _physics_process(delta):
 			velocity.y = terminalVelocity
 
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = -jump_force
+		velocity.y = -jump_force 
 
 	var hori_direction = Input.get_axis("move_left", "move_right")
+	
+	if hori_direction > 0:
+		sprite.flip_h = false
+	elif hori_direction <0:
+		sprite.flip_h = true
+	
+	if Input.is_action_just_pressed("possess"):
+		animator.play("throw")
+		print("hi")
+	if Input.is_action_just_released("possess"):
+		animator.play("release")
+		
+
 	velocity.x = speed * hori_direction  
 
 	# Flip sprite depending on direction
