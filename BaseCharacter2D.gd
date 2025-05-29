09017ad6ba61fe9dbd2f54a7345b6f011c767231
strubium <113206902+strubium@users.noreply.github.com>
2025-05-29 13:56:53 -0500

    has_jump

--- BaseCharacter2D.gd
+++ BaseCharacter2D.gd
@@ -5,6 +5,7 @@ class_name BaseCharacter2D
 @export var gravity: float = 1200
 @export var terminal_velocity: float = 1800
 @export var jump_force: float = 1000
+@export var has_jump: bool = false
 
 func apply_gravity(delta: float) -> void:
 	if not is_on_floor():
@@ -12,7 +13,7 @@ func apply_gravity(delta: float) -> void:
 		velocity.y = min(velocity.y, terminal_velocity)
 
 func handle_jump() -> void:
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if has_jump and Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = -jump_force
 
 func get_horizontal_input() -> float:
