15fea10278ef3963890937294581a974400ee755
strubium <113206902+strubium@users.noreply.github.com>
2025-06-01 20:23:56 -0500

    music!

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,7 +1,12 @@
 extends Control
 
+@onready var ost_player = $AudioStreamPlayer2D
+
+func _ready():
+	ost_player.play()
+
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/levelselect.tscn") 
-
+	
 func _on_quit_button_pressed():
 	get_tree().quit()
